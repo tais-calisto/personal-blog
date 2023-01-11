39d5bea29d883fef6f05232d91351fcd1ec08ff0
Tais Calisto <taiscalisto97@gmail.com>
2023-01-10 21:37:09 -0300

    Corrected detect system theme functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { IconBrightnessUp, IconSunOff } from '@tabler/icons';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { ToggleStyle } from '../styles/toggleTheme.style';
 import { GlobalStyle } from '../styles/global';
 import { lightTheme, darkTheme } from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { matches } = window.matchMedia('(prefers-color-scheme: dark)');
-
-  const [isDarkTheme, setDarkTheme] = useState(matches);
+  const [isDarkTheme, setDarkTheme] = useState(false);
+  useEffect(() => {
+    const { matches } = window.matchMedia('(prefers-color-scheme: dark)');
+    setDarkTheme(matches);
+  }, []);
 
   const toggleTheme = () => {
     setDarkTheme(!isDarkTheme);
