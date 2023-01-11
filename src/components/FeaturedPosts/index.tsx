9b5997bc8550ca7dfe7a1b88d63a40111dbd89b8
Tais Calisto <taiscalisto97@gmail.com>
2023-01-10 23:58:16 -0300

    Added getPost()

--- src/components/FeaturedPosts/index.tsx
+++ src/components/FeaturedPosts/index.tsx
@@ -3,12 +3,19 @@ import { Container } from './style';
 import PostImage from '../PostImage';
 import PostCategory from '../PostCategory';
 import PostDate from '../PostDate';
+import { useRouter } from 'next/router';
 
 const FeaturedPosts = ({ posts }: { posts: Array<Posts> }) => {
+  const router = useRouter();
+
+  const getPost = (path: string) => {
+    router.push(`post/${path}`);
+  };
+
   return (
     <Container>
       {posts.map((p) => (
-        <article key={p.id}>
+        <article key={p.id} onClick={() => getPost(p.path)}>
           <PostImage path={p.image} />
           <div className='content'>
             <PostCategory id={p.category_id} />
