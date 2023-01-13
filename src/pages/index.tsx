7395e3303413280bed08846bcaff1142dd738767
Tais Calisto <taiscalisto97@gmail.com>
2023-01-13 17:32:28 -0300

    Updated favicon icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home({
           content='Informações, receitas e conteúdos sobre veganismo, vegetarianismo e saúde'
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/favicon.png' />
       </Head>
       <HomeStyle>
         <Header data={categories} />
